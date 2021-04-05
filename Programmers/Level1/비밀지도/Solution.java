ef31663e3a56c93d51398d8ab7720e3201ffba5c
ChaeEun Jeong <35367660+fkakd1205@users.noreply.github.com>
2021-04-05 18:36:43 +0900

    modify comment

--- Programmers/Level1/비밀지도/Solution.java
+++ Programmers/Level1/비밀지도/Solution.java
@@ -8,7 +8,7 @@ class Solution {
         }
         
         for(int i = 0; i < n; i++){
-            //문자열 자릿수 지정(%n), 자릿수를 지정하지 않으면 문자열 첫번째에 공백이 들어왔을 때 출력 x
+            //문자열 자릿수 지정("%" + n + "s"), 자릿수를 지정하지 않으면 문자열 첫번째에 공백이 들어왔을 때 출력 x
             answer[i] = String.format("%" + n + "s", answer[i]); 
             answer[i] = answer[i].replace("1", "#");
             answer[i] = answer[i].replace("0", " ");
