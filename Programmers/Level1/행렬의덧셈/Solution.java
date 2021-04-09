e7e3bbea54b58e60d9e3fddc7ff2b7ebf7a0a3f7
ChaeEun Jeong <35367660+fkakd1205@users.noreply.github.com>
2021-04-09 17:40:51 +0900

    modify comment

--- Programmers/Level1/행렬의덧셈/Solution.java
+++ Programmers/Level1/행렬의덧셈/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1[0].length];  // arr1과 arr2의 행과 열 크기 동일하므로, 둘 중 하나의 행과 열 크기로 answer크기 지정. (arr.length - 행 크기, arr[0].length - 열 크기)
-        
+        int[][] answer = new int[arr1.length][arr1[0].length];  // arr1과 arr2의 행과 열 크기 동일하므로, 둘 중 하나의 행과 열 크기로 answer크기 지정.
+                                                                // arr1.length - arr1의 행 크기, arr1[0].length - arr1의 열 크기 (이때, arr1은 정방형 배열)
         for(int i = 0; i < arr1.length; i++){
             for(int j = 0; j < arr1[0].length; j++){
                 answer[i][j] = arr1[i][j] + arr2[i][j];
