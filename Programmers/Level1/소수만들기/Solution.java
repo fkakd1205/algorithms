8238a512df417b5168df2d01a40191e8de0beefd
fkakd1205 <fkakd0102@naver.com>
2021-04-11 18:44:14 +0900

    solution 소수 만들기

---
+++ Programmers/Level1/소수만들기/Solution.java
@@ -0,0 +1,30 @@
+package level1;
+
+class Solution41 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;	// 세 수를 더한 값을 저장하기 위한 변수
+        
+        // 서로 다른 3개의 수를 더해서 소수인지 판별
+        for(int i = 0; i < nums.length-2; i++){
+            for(int j = i+1; j < nums.length-1; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    // 소수인지 판별
+    public boolean isPrime(int sum){
+            boolean flag = true;
+            
+            for(int i = 2; i < sum; i++){
+                if(sum % i == 0) flag = false;  // 소수가 아니면
+            }
+            
+            return flag;
+    }
+}
