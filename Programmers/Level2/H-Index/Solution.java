6dd922b4db22c182d294dc889da3f8d20d865eb0
DESKTOP-H9G9PSJ\정채은 <fkakd1205@gmail.com>
2021-11-02 23:47:00 +0900

    Use Arrays.sort() - H-Index

--- Programmers/Level2/H-Index/Solution.java
+++ Programmers/Level2/H-Index/Solution.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i = 0; i < citations.length; i++) {
             int hIdx = citations.length - i;        // H-Index
             
-            // 배열을 오름차순 정렬했으므로, 
+            // 배열을 오름차순 정렬했으므로, hIdex보다 현재 요소의 값이 크다면 더이상 실행하지 X
             if(hIdx <= citations[i]) {
                 answer = hIdx;
                 break;
