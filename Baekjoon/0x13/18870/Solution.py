a70aadcf294fe0381d467f96372770e8c1788a51
fkakd1205 <fkakd0102@naver.com>
2024-01-31 18:33:30 +0900

    update solution 0x13 - 18870

--- Baekjoon/0x13/18870/Solution.py
+++ Baekjoon/0x13/18870/Solution.py
@@ -1,27 +1,20 @@
 from sys import stdin
-from copy import deepcopy
 
 N = int(input())
 X = list(map(int, stdin.readline().split()))
 
-# temp1 : 정렬된 X, temp2 : 중복제거된 temp1
-temp1 = deepcopy(X)
-temp1.sort()
-temp2 = []
-
-# 중복 제거
-for i in range(len(temp1)):
-    if((not temp2) or temp1[i-1] != temp1[i]):
-        temp2.append(temp1[i])
+# temp : 중복제거, 정렬된 X
+temp = list(set(X))
+temp.sort()
 
 def binary_search(target):
     st = 0
-    en = len(temp2)-1
+    en = len(temp)-1
 
     while(st <= en):
         mid = (st + en) // 2
-        if(temp2[mid] < target): st = mid + 1
-        elif(temp2[mid] > target): en = mid - 1
+        if(temp[mid] < target): st = mid + 1
+        elif(temp[mid] > target): en = mid - 1
         else: return mid
     return 0
 
